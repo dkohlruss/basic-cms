645cb493fcd75f8f9a5f77e6836d0e84a421ee39
dkohlruss <dkohlruss@gmail.com>
2017-04-24 16:58:21 -0600

    Heroku DB config

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost'); /*Database Server*/
-define('DB_NAME', 'WEBADMIN'); /*Database Name*/
-define('DB_USER', 'root'); /*Database Username*/
-define('DB_PWD',  ''); /*Database Password*/
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'); /*Database Server*/
+define('DB_NAME', 'heroku_ad9bf0e402e85c2'); /*Database Name*/
+define('DB_USER', 'b29d7d77ba8fa8'); /*Database Username*/
+define('DB_PWD',  'd8664449'); /*Database Password*/
 ?>
