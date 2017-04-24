1b800208e3f0791c233d8e21cdc8f4fed4aab2d5
dkohlruss <dkohlruss@gmail.com>
2017-04-24 17:28:03 -0600

    new user redirect

--- php/config.php
+++ php/config.php
@@ -1,9 +1,7 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-define('DB_HOST', substr($url["path"], 1)); /*Database Server*/
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'); /*Database Server*/
 define('DB_NAME', 'heroku_c0930732318d46a'); /*Database Name*/
-define('DB_USER', $url["user"]); /*Database Username*/
-define('DB_PWD',  $url["pass"]); /*Database Password*/
+define('DB_USER', 'bdda5995a40e22'); /*Database Username*/
+define('DB_PWD',  'c283f743'); /*Database Password*/
 ?>
