b1aec674656495d7c64a42dacf9120e383666b14
dkohlruss <dkohlruss@gmail.com>
2017-04-24 21:23:34 -0600

    adjust css

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($result = mysqli_query($link,$sql)) {
     <script src="./js/index.js" async></script>
   </head>
   <body>
-    <header class="title-bar">
+    <header class="container title-bar">
       <h1 class="title-text"><?= $siteTitle; ?></h1>
       <nav class="navbar">
         <ul class="nav-list">
