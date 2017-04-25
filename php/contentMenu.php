2c51e1806412ca7660524eac9a42a37389c0d872
dkohlruss <dkohlruss@gmail.com>
2017-04-24 22:29:24 -0600

    css edit for content menu

--- php/contentMenu.php
+++ php/contentMenu.php
@@ -17,6 +17,11 @@ $sql = "SELECT Nav_ID, Nav_Title, Display_Order FROM Nav ORDER BY Display_Order,
     <h1 class="center_text">Site Configuration</h1>
     <div class="content">
       <a href="contentNew.php">Create new content</a>
+      <div class="config_entry">
+        <div class="config_name">Order</div>
+        <div class="text_value">Title</div>
+        <div class="modify">Modify</div>
+      </div>
         <?php
         try {
           if ($result = mysqli_query($link, $sql)) {
@@ -24,9 +29,9 @@ $sql = "SELECT Nav_ID, Nav_Title, Display_Order FROM Nav ORDER BY Display_Order,
             ?>
             <form action="contentEdit.php" method="POST">
               <input type="hidden" name="Nav_ID" value="<?= $row['Nav_ID']; ?>" />
-              <div><input type="text" name="Display_Order" value="<?= $row['Display_Order'] ?>" /></div>
-              <div><?= $row['Nav_Title'] ?></div>
-              <input type="submit" value="Edit" />
+              <div class="config_name"><?= $row['Display_Order'] ?></div>
+              <div class="text_value"><?= $row['Nav_Title'] ?></div>
+              <div class="modify"><input type="submit" value="Edit" /></div>
             </form> <?php
           }
         }
