44ab9a2184787d15b2d63614546eda1d9c56aca2
dkohlruss <dkohlruss@gmail.com>
2017-04-24 22:36:23 -0600

    css edit

--- php/contentEdit.php
+++ php/contentEdit.php
@@ -28,7 +28,7 @@ $displayorder = $_POST['Display_Order'];
             <form action="contentEditUpdate.php" method="POST">
               <input type="hidden" name="Content_ID" value="<?= $id; ?>" />
               <input type="text" name="Display_Order" value="<?= $displayorder; ?>" width="75" /> <br>
-              <textarea name="ContentTitle" cols="100" rows="20"><?= $row['ContentTitle'] ?></textarea>
+              <textarea name="ContentTitle" cols="150" rows="20"><?= $row['ContentTitle'] ?></textarea>
               <input type="text" name="Content" value="<?= $row['Content'] ?>" />
               <input type="submit" value="Update" />
             </form>
--- php/contentNew.php
+++ php/contentNew.php
@@ -14,8 +14,8 @@ require('SQLFunctions.php');
     <div class="content">
         <h3 class="center_text">Create New Content</h3>
         <form action="contentNewSubmit.php" method="POST">
-            <input type="text" name="ContentTitle" />
-            <input type="textarea" name="Content" />
+            <input type="text" name="ContentTitle" width="75" />
+            <textarea name="Content" cols="150" rows="20" /> </textarea>
             <input type="submit" value="Create" />
         </form>
     </div>
