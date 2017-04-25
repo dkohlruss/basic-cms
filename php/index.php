51ade93eaf64d27f24b935a9c11ad8476f6fa14e
dkohlruss <dkohlruss@gmail.com>
2017-04-24 22:27:07 -0600

    css edit

--- php/index.php
+++ php/index.php
@@ -10,7 +10,7 @@ require('session.php');
     <link href="../css/main.css" rel="stylesheet">
   </head>
   <body>
-    <h1>Simple CMS</h1>
+    <h1 class="center_text">Simple CMS</h1>
     <div class="row">
         <div class="content"><a href="./settingsMenu.php">Settings</a></div>
         <div class="content"><a href="./contentMenu.php">Content</a></div>
