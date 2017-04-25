0d63583ee9e84b8c0426811fec055640b63abe87
dkohlruss <dkohlruss@gmail.com>
2017-04-24 20:22:45 -0600

    Edit nav order

--- php/session.php
+++ php/session.php
@@ -11,13 +11,12 @@
         } else {
             $_SESSION['timeout'] = time();
             echo "<div align='right'>";
-             echo "  <a href='AdminIndex.php'>Preview</a>";
-             echo "  <a href='index.php'>Home</a>";
-             echo "  <a href='logout.php'>Log Out</a>";
-             echo "  <a href='signup.php'>Create User</a>";
-             echo "  <a href='EditNavMenu.php'>Navigation Menu</a>";
-             echo "  <a href='EditContentMenu.php'>Content Menu</a>";
-             echo "  <a href='SiteConfigMenu.php'>Site Config Menu</a>";
+             echo "  <a href='AdminIndex.php'>Preview</a> | " ;
+             echo "  <a href='index.php'>Home</a> | ";
+             echo "  <a href='logout.php'>Log Out</a> | ";
+             echo "  <a href='signup.php'>Create User</a> | ";
+             echo "  <a href='contentMenu.php'>Content Menu</a> | ";
+             echo "  <a href='settingsMenu.php'>Site Config Menu</a>";
              echo "</div>";
              if (isset($_SESSION['message'])) {
                  echo "<h3>" . $_SESSION['message'] . "</h3>";
