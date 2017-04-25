9b7687e6833a76b39eb8a526b4c0a44de8a74cbf
dkohlruss <dkohlruss@gmail.com>
2017-04-24 19:07:13 -0600

    removed line that got in there by the power of mysteries

--- php/contentNewSubmit.php
+++ php/contentNewSubmit.php
@@ -25,23 +25,19 @@ try {
 }
 
 try {
-    $navsqlinsert = "INSERT INTO Nav (Nav_Title, Display_Order) VALUES ('" . $title . "','" . $order . "');";
-    if (mysqli_query($link,$navsqlinsert)) {
-      $navsqlquery = "SELECT 1 FROM Nav ORDER BY Nav_ID DESC;";
-      if ($result = mysqli_query($link,$navsqlquery)) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          $navid = $row['Nav_ID'];
-        }
-        $contentsql = "INSERT INTO Content (Nav_ID, ContentTitle, Content) VALUES ('" . $navid . "','" . $title . "','" . $content . "');";
-        if (mysqli_query($link, $contentsql)) {
-          $message = "New record successfully created!";
-        }
+  $navsqlinsert = "INSERT INTO Nav (Nav_Title, Display_Order) VALUES ('" . $title . "','" . $order . "');";
+  if (mysqli_query($link,$navsqlinsert)) {
+    $navsqlquery = "SELECT 1 FROM Nav ORDER BY Nav_ID DESC;";
+    if ($result = mysqli_query($link,$navsqlquery)) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $navid = $row['Nav_ID'];
+      }
+      $contentsql = "INSERT INTO Content (Nav_ID, ContentTitle, Content) VALUES ('" . $navid . "','" . $title . "','" . $content . "');";
+      if (mysqli_query($link, $contentsql)) {
+        $message = "New record successfully created!";
       }
     }
-
-
-
-    $contentsql = "INSERT INTO Content (Nav_ID)"
+  }
 } catch (Exception $e) {
   $message = $e;
 }
