d592c19ad2390e36a33e2f08243247349741dd91
dkohlruss <dkohlruss@gmail.com>
2017-04-24 22:32:10 -0600

    css edit for content menu

--- php/contentEdit.php
+++ php/contentEdit.php
@@ -27,7 +27,7 @@ $displayorder = $_POST['Display_Order'];
             ?>
             <form action="contentEditUpdate.php" method="POST">
               <input type="hidden" name="Content_ID" value="<?= $id; ?>" />
-              <input type="text" name="Display_Order" value="<?= $displayorder; ?>" /> <br>
+              <input type="text" name="Display_Order" value="<?= $displayorder; ?>" width="75" /> <br>
               <textarea name="ContentTitle" cols="100" rows="20"><?= $row['ContentTitle'] ?></textarea>
               <input type="text" name="Content" value="<?= $row['Content'] ?>" />
               <input type="submit" value="Update" />
